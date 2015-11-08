ce775649a69ae6a024b043922e90e43b21dc4252
cunnia3 <andrew64ce@gmail.com>
2015-11-08 14:44:39 -0500

    Changed connect port to defualt port

--- RR_server/arduinoNRFClient.py
+++ RR_server/arduinoNRFClient.py
@@ -9,9 +9,10 @@ def main():
     t2 = RR.TcpTransport()
     RRN.RegisterTransport(t2)
 
-    myGateway = RRN.ConnectService('tcp://localhost:40493/arduinoIMU/arduinoIMUData')
+    myGateway = RRN.ConnectService('tcp://localhost:10001/arduinoIMU/arduinoIMUData')
 
     print myGateway.IMU1_read() 
+    print myGateway.IMU2_read() 
 
     RRN.Shutdown()
   
