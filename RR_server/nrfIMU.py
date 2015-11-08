2287dacdbb7a3b674708dd56c075b12bd3e486bc
cunnia3 <andrew64ce@gmail.com>
2015-11-08 14:44:05 -0500

    Added ability to specify usb port over command line

--- RR_server/nrfIMU.py
+++ RR_server/nrfIMU.py
@@ -9,25 +9,21 @@ import traceback
 import socket
 import time
 import sys
-
+import signal
+import argparse
 
 RRN = RR.RobotRaconteurNode.s
 
-serial_port_name="/dev/ttyACM0"
-
-def get_open_port():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('127.0.0.1', 0))
-    port = sock.getsockname()
-    sock.close()
-    time.sleep(3)
-    return port[1]
+# catch control c to allow for use of the same port
+def signal_handler(signal, frame):
+        print('Ctrl+C shutdown!')
+        RRN.Shutdown()
+        sys.exit(0)
 
 class NrfImuInterface(object):
-    def __init__(self):
+    def __init__(self,  serialPort):
         self._lock=threading.RLock()
-        self._serial=serial.Serial(serial_port_name,baudrate=57600)
-
+        self._serial=serial.Serial(serialPort,baudrate=57600)
 
     def IMU1_read(self):
         with self._lock:
@@ -35,7 +31,7 @@ class NrfImuInterface(object):
             self._serial.write(dat)
             raw = self._serial.read(size = 14)
             unConverted = struct.unpack( "hhhhhhh", raw )
-            converted = [unConverted[0]/16384.0, unConverted[1]/16384.0,unConverted[2]/16384.0,unConverted[3]/131.0,unConverted[4]/131.0,unConverted[5]/131]
+            converted = [unConverted[0]/16384.0, unConverted[1]/16384.0,unConverted[2]/16384.0,unConverted[3]/131.0,unConverted[4]/131.0,unConverted[5]/131.0]
             return converted
 
     def IMU2_read(self):
@@ -44,18 +40,28 @@ class NrfImuInterface(object):
             self._serial.write(dat)
             raw = self._serial.read(size = 14)
             unConverted = struct.unpack( "hhhhhhh", raw )
-            converted = [unConverted[0]/16384.0, unConverted[1]/16384.0,unConverted[2]/16384.0,unConverted[3]/131.0,unConverted[4]/131.0,unConverted[5]/131]
+            converted = [unConverted[0]/16384.0, unConverted[1]/16384.0,unConverted[2]/16384.0,unConverted[3]/131.0,unConverted[4]/131.0,unConverted[5]/131.0]
             return converted
 		     
 
 def main():
+    # accept arguments to command line call
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--serverPort", help="port to start this server on")
+    parser.add_argument("--roboclawUSBPort", help="USB port that the roboclaw is on")
+    args = parser.parse_args()
+    port = args.serverPort
+    usbPort = args.roboclawUSBPort
+    if port == None:
+        port = 10001
+    
+    if usbPort == None:
+        usbPort = '/dev/ttyACM1'
+        
     t1 = RR.LocalTransport()
     t1.StartServerAsNodeName("arduinoIMU")
     RRN.RegisterTransport(t1)
-    
 
-    port = get_open_port()
-    print "Accepting commands on port: " + str(port)
     t2 = RR.TcpTransport()
     t2.EnableNodeAnnounce()
     t2.StartServer(port)
@@ -64,11 +70,11 @@ def main():
     with open('nrfIMU.robodef', 'r') as f:
         service_def = f.read()
     
-    myGateway = NrfImuInterface()
+    myGateway = NrfImuInterface(usbPort)
 
     RRN.RegisterServiceType(service_def)
     RRN.RegisterService("arduinoIMUData", "arduinoIMU.NrfImuInterface", myGateway)
-
+    print "Accepting commands on port: " + str(port)
     raw_input("Press any key to end")
 
     RRN.Shutdown()
